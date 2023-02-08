db27c0d760b42cde7e693094a14f92b623a36da5
IdoGalil <IdoGalil@users.noreply.github.com>
2023-02-08 10:48:13 +0200

    Mismatch between keys from the main file

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -57,7 +57,7 @@ def create_dataset_metadata(dataset_info_dict, is_id_dataset=False):
         train_idx = None
     else:
 
-        percentage = dataset_info_dict['percentage']
+        percentage = dataset_info_dict['test_estimation_split_percentage']
         train_idx, val_idx = split_dataset(labels, percentage)
 
     meta_data = {'image_files': image_files, 'labels': labels, 'class_names': class_names,
