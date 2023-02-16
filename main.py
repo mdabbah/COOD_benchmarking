ccc9d7fda33be371b99e2f3bf205e8172c1e1abc
IdoGalil <IdoGalil@users.noreply.github.com>
2023-02-16 17:51:37 +0200

    Updated main.py to replicate the bug concerning passing the model as a dict

--- main.py
+++ main.py
@@ -15,24 +15,48 @@ if __name__ == '__main__':
     #     'dataset_name': 'ImageNet_1K',
     #     'images_base_folder': '<path to images dir>',
     # }
+
+    from download_dummy_dataset import download_dummy_dataset
+    download_dummy_dataset('../test_COOD')
+
     dummy_ood_dataset_info = {
         'dataset_name': 'Dummy_OOD',
-        # 'images_base_folder': '.\dummy_dataset\dummy_ood',  # <path to images dir>/classname/*.(jpg|png|jpeg)
-        'images_base_folder': '/media/mohammed/Elements1/exported_datasets/dummy_ood',  # <path to images dir>/classname/*.(jpg|png|jpeg)
+        'images_base_folder': '..\dummy_dataset\dummy_ood',  # <path to images dir>/classname/*.(jpg|png|jpeg)
+        # 'images_base_folder': '/media/mohammed/Elements1/exported_datasets/dummy_ood',  # <path to images dir>/classname/*.(jpg|png|jpeg)
         'test_estimation_split_percentage': 0.25
     }
 
     dummy_id_dataset_info = {
         'dataset_name': 'Dummy_ID',
-        # 'images_base_folder': '.\dummy_dataset\dummy_id',
-        'images_base_folder': '/media/mohammed/Elements1/exported_datasets/dummy_id'
+        'images_base_folder': '..\dummy_dataset\dummy_id',
+        # 'images_base_folder': '/media/mohammed/Elements1/exported_datasets/dummy_id'
     }
 
-    results2 = benchmark_model_on_cood_with_severities(model=['resnet50', 'resnet18'],
-                                                       confidence_metric=['softmax', 'entropy'],
-                                                       cood_dataset_info=dummy_ood_dataset_info,
-                                                       id_dataset_info=dummy_id_dataset_info)
-    plotly.express.line(results2, x='severity_level', y='ood-auroc', color='model_name-kappa')
+
+    import torchvision.transforms as tvtf
+    from torchvision.models import resnet50
+    resnet50_transform = tvtf.Compose([tvtf.Resize(256), tvtf.CenterCrop(224), tvtf.ToTensor(),
+                                       tvtf.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    example_model_input = {'model_name': 'resnet50', 'model': resnet50(),
+                           'transforms': resnet50_transform}  # recommended
+
+    from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
+    weights = MobileNet_V3_Small_Weights.DEFAULT
+    transforms = weights.transforms()
+    model = mobilenet_v3_small(weights)
+    # Note to mohammed: the problem persists even if instead of transforms you'd use resnet50_transform for example
+    # example_model_input = {'model_name': 'mobilenet_v3_small', 'model': model, 'transforms': transforms}
+    results = benchmark_model_on_cood_with_severities(model=example_model_input,
+                                                      confidence_metric='softmax',
+                                                      cood_dataset_info=dummy_ood_dataset_info,
+                                                      id_dataset_info=dummy_id_dataset_info)
+    plotly.express.line(results, x='severity_level', y='ood-auroc', color='model_name-kappa')
+
+    # results2 = benchmark_model_on_cood_with_severities(model=['resnet50', 'resnet18'],
+    #                                                    confidence_metric=['softmax', 'entropy'],
+    #                                                    cood_dataset_info=dummy_ood_dataset_info,
+    #                                                    id_dataset_info=dummy_id_dataset_info)
+    # plotly.express.line(results2, x='severity_level', y='ood-auroc', color='model_name-kappa')
 
 
     # results = benchmark_model_on_cood_with_severities(model=['resnet50', 'resnet18'],
@@ -50,11 +74,6 @@ if __name__ == '__main__':
     #
     # #
     #
-    # from torchvision.models import resnet50
-    # import torchvision.transforms as tvtf
-    #
-    # resnet50_transform = tvtf.Compose([tvtf.Resize(256), tvtf.CenterCrop(224), tvtf.ToTensor(),
-    #                                    tvtf.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
     #
     # example_model_input = {'model_name': 'resnet50', 'nn.Module': resnet50(),
     #                        'nn.transforms': resnet50_transform}  # recommended
